e5008a2a73b70e1f1fab8c8a6d207cd165486505
KishorPokharel <kishorpokharel7@gmail.com>
2022-07-27 20:21:48 +0545

    fix: make escape stay on same dir

--- shellrc.sh
+++ shellrc.sh
@@ -3,6 +3,9 @@ mktouch() {
     mkdir -p "$(dirname "$1")"
     touch "$1"
 }
+sd() {
+    cd ${"$(fd --type d --exclude node_modules | fzf)":-"."}
+}
 yt2mp3() {
     youtube-dl -x --audio-format mp3 $1 --no-check-certificate
 }
@@ -11,5 +14,4 @@ alias lspath='echo $PATH | tr ":" "\n"'
 alias q='exit'
 alias vimrc="vi ~/.vimrc"
 alias zshrc="vi ~/.zshrc"
-alias sd="cd \$(fd --type d --exclude node_modules | fzf)"
 alias cat=bat
